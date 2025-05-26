80bc5cc6ee2a620b7253208ef0086eb3523a0f96
00018400 <00018400aa@gmail.com>
2025-05-26 15:37:55 +0500

    Initial commit: added C code

---
+++ Print-F-and-C-.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void bigFandC() {
+    printf("\n#####\n#\n#\n####\n#\n#\n#\n");
+    printf("    #####  \n  ##     ##\n#\n#\n#\n#\n#\n  ##     ##\n    #####  ");
+}
+
+int main(){
+    bigFandC();
+}
